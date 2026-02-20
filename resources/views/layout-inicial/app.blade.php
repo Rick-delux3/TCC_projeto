d601114730d78772e87f425c2c2735e255a1d27d
Rick-delux3 <henriquerodriguesneves4@gmail.com>
2026-02-20 08:53:11 -0300

    tradução modais

--- resources/views/layout-inicial/app.blade.php
+++ resources/views/layout-inicial/app.blade.php
@@ -57,8 +57,8 @@
                 <div class="modal-dialog modal-dialog-centered">
                     <div class="modal-content">
                     <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="staticBackdropLabel">Sucesso!!</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <h1 class="modal-title fs-5" id="staticBackdropLabel">Sucesso</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
                     </div>
                     <div class="modal-body">
                        {{ session('success') }}
